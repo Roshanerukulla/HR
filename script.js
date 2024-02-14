599cfa31e8b5c76af5e9e5cc0c17a52187cfcca9
Roshanerukulla <125940568+Roshanerukulla@users.noreply.github.com>
2024-02-14 01:41:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const heart = document.getElementById('heart');
-const loveMessage = document.getElementById('love-message');
 
-heart.addEventListener('click', () => {
-  heart.classList.add('explode');
-  loveMessage.style.display = 'block';
-});
-
-setTimeout(() => {
-  heart.classList.remove('explode');
-}, 1000); // Adjust explosion duration
